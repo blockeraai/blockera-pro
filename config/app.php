6548d88643041d6b7f5da194c562c2467c6147ff
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-04 11:14:36 +0330

    feat: publish config files

---
+++ config/app.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	'name'          => 'blockera-pro',
+	'root_url'      => BLOCKERA_PRO_URI,
+	'root_path'     => BLOCKERA_PRO_PATH,
+	'packages_url'  => BLOCKERA_PRO_URI . 'packages/',
+	'packages_path' => BLOCKERA_PRO_PATH . 'packages/',
+	'version'       => blockera_core_env( 'VERSION' ),
+	'namespaces'    => [
+		'controllers' => '\Blockera\Pro\Http\Controllers\\',
+	],
+	'debug'         => blockera_core_env( 'APP_MODE' ) && 'development' === blockera_core_env( 'APP_MODE' ) || ( ( defined( 'WP_DEBUG' ) && WP_DEBUG ) ),
+	'providers'     => [
+		\Blockera\Pro\Providers\AppServiceProvider::class,
+		\Blockera\Pro\Providers\AssetProvider::class,
+	],
+];
