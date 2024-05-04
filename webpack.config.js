39bb3a56e9b4a74d0c94065f6882cd47822b08ec
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-04 11:12:22 +0330

    chor: add config files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+const path = require('path');
+
+/**
+ * Internal dependencies
+ */
+const packagesConfig = require('./packages/dev-tools/js/webpack/packages');
+
+module.exports = (env, argv) => {
+	if (!env) {
+		return require(path.resolve(
+			process.cwd(),
+			'./packages/dev-tools/js/cypress/webpack.config.js'
+		));
+	}
+
+	return packagesConfig(env, { ...argv, mode: argv?.mode || 'production' });
+};
