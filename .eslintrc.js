9e8f0a0e0c2f830746dafd1a3934cd197552e735
rezaelahidev <rezaelahi.dev@gmail.com>
2024-09-28 13:03:32 +0330

    chore: update the .eslnitrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,51 @@
+const restrictedImports = [
+	{
+		name: 'lodash-es',
+		message:
+			'This Lodash is not recommended. Please use native functionality instead.',
+	},
+	{
+		name: 'lodash',
+		message:
+			'This Lodash is not recommended. Please use native functionality instead.',
+	},
+	{
+		name: 'classnames',
+		message:
+			"Please use `classNames` function or other functions from `@blockera/classnames` instead. It's a lighter, faster and is compatible all Blockera packages.",
+	},
+	{
+		name: 'clx',
+		message:
+			"Please use `classNames` function or other functions from `@blockera/classnames` instead. It's a lighter, faster and is compatible all Blockera packages.",
+	},
+	{
+		name: 'clsx',
+		message:
+			"Please use `classNames` function or other functions from `@blockera/classnames` instead. It's a lighter, faster and is compatible all Blockera packages.",
+	},
+	{
+		name: 'prop-types',
+		message:
+			'`prop-types` is not recommended. Please use TypeScript or Flow.js for type checking.',
+	},
+	{
+		name: 'framer-motion',
+		message:
+			'Please use the Framer Motion API through `@wordpress/components` instead. (__unstableMotion, __unstableAnimatePresence and __unstableMotionContext)',
+	},
+	{
+		name: '@ariakit/react',
+		message:
+			'Please use Ariakit API through `@wordpress/components` instead.',
+	},
+	{
+		name: '@ariakit/react',
+		message:
+			'Please use Ariakit API through `@wordpress/components` instead. (__unstableComposite, __unstableCompositeGroup, __unstableCompositeItem and __unstableUseCompositeState)',
+	},
+];
+
 module.exports = {
 	parser: 'hermes-eslint',
 	extends: [
@@ -31,7 +79,36 @@ module.exports = {
 		],
 		'jsx-a11y/no-static-element-interactions': 'off',
 		'jsx-a11y/click-events-have-key-events': 'off',
+		'no-restricted-imports': [
+			'error',
+			{
+				paths: restrictedImports,
+			},
+		],
+		'@wordpress/i18n-text-domain': [
+			'error',
+			{
+				allowedTextDomain: ['blockera-pro'],
+			},
+		],
 	},
+	overrides: [
+		{
+			files: ['packages/classnames/js/**'],
+			rules: {
+				'no-restricted-imports': [
+					'error',
+					// The `clsx` us used inside the `@blockera/classnames`, hence why importing this
+					// dependency should be allowed in the classnames package.
+					{
+						paths: restrictedImports.filter(
+							({ name }) => 'clsx' !== name
+						),
+					},
+				],
+			},
+		},
+	],
 	env: {
 		jest: true,
 	},
