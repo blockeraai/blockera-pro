9a698919a8bd79ee135a83b8d64b544558f50e28
rezaelahidev <rezaelahi.dev@gmail.com>
2024-10-03 10:53:31 +0330

    style: fix js coding standards

--- packages/controls-pro/js/transition-control/utils.js
+++ packages/controls-pro/js/transition-control/utils.js
@@ -9,234 +9,234 @@ export const getTransitionTypeOptions = function (): Array<Object> {
 	return [
 		{
 			type: 'optgroup',
-			label: __('Common Transitions', 'blockera'),
+			label: __('Common Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('All Properties', 'blockera'),
+					label: __('All Properties', 'blockera-pro'),
 					value: 'all',
 				},
 				{
-					label: __('Opacity', 'blockera'),
+					label: __('Opacity', 'blockera-pro'),
 					value: 'opacity',
 				},
 				{
-					label: __('Margin', 'blockera'),
+					label: __('Margin', 'blockera-pro'),
 					value: 'margin',
 				},
 				{
-					label: __('Padding', 'blockera'),
+					label: __('Padding', 'blockera-pro'),
 					value: 'padding',
 				},
 				{
-					label: __('Border', 'blockera'),
+					label: __('Border', 'blockera-pro'),
 					value: 'border',
 				},
 				{
-					label: __('Transform', 'blockera'),
+					label: __('Transform', 'blockera-pro'),
 					value: 'transform',
 				},
 				{
-					label: __('Filter', 'blockera'),
+					label: __('Filter', 'blockera-pro'),
 					value: 'filter',
 				},
 				{
-					label: __('Flex', 'blockera'),
+					label: __('Flex', 'blockera-pro'),
 					value: 'flex',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Background Transitions', 'blockera'),
+			label: __('Background Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Background Color', 'blockera'),
+					label: __('Background Color', 'blockera-pro'),
 					value: 'background-color',
 				},
 				{
-					label: __('Background Position', 'blockera'),
+					label: __('Background Position', 'blockera-pro'),
 					value: 'background-position',
 				},
 				{
-					label: __('Text Shadow', 'blockera'),
+					label: __('Text Shadow', 'blockera-pro'),
 					value: 'text-shadow',
 				},
 				{
-					label: __('Box Shadow', 'blockera'),
+					label: __('Box Shadow', 'blockera-pro'),
 					value: 'box-shadow',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Size Transitions', 'blockera'),
+			label: __('Size Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Width', 'blockera'),
+					label: __('Width', 'blockera-pro'),
 					value: 'width',
 				},
 				{
-					label: __('Height', 'blockera'),
+					label: __('Height', 'blockera-pro'),
 					value: 'height',
 				},
 				{
-					label: __('Max Height', 'blockera'),
+					label: __('Max Height', 'blockera-pro'),
 					value: 'max-height',
 				},
 				{
-					label: __('Max Width', 'blockera'),
+					label: __('Max Width', 'blockera-pro'),
 					value: 'max-width',
 				},
 				{
-					label: __('Min Height', 'blockera'),
+					label: __('Min Height', 'blockera-pro'),
 					value: 'min-height',
 				},
 				{
-					label: __('Min Width', 'blockera'),
+					label: __('Min Width', 'blockera-pro'),
 					value: 'min-width',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Borders Transitions', 'blockera'),
+			label: __('Borders Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Border Radius', 'blockera'),
+					label: __('Border Radius', 'blockera-pro'),
 					value: 'border-radius',
 				},
 				{
-					label: __('Border Color', 'blockera'),
+					label: __('Border Color', 'blockera-pro'),
 					value: 'border-color',
 				},
 				{
-					label: __('Border Width', 'blockera'),
+					label: __('Border Width', 'blockera-pro'),
 					value: 'border-width',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Typography Transitions', 'blockera'),
+			label: __('Typography Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Font Color', 'blockera'),
+					label: __('Font Color', 'blockera-pro'),
 					value: 'color',
 				},
 				{
-					label: __('Font Size', 'blockera'),
+					label: __('Font Size', 'blockera-pro'),
 					value: 'font-size',
 				},
 				{
-					label: __('Line Height', 'blockera'),
+					label: __('Line Height', 'blockera-pro'),
 					value: 'line-height',
 				},
 				{
-					label: __('Letter Spacing', 'blockera'),
+					label: __('Letter Spacing', 'blockera-pro'),
 					value: 'letter-spacing',
 				},
 				{
-					label: __('Text Indent', 'blockera'),
+					label: __('Text Indent', 'blockera-pro'),
 					value: 'text-indent',
 				},
 				{
-					label: __('Word Spacing', 'blockera'),
+					label: __('Word Spacing', 'blockera-pro'),
 					value: 'word-spacing',
 				},
 				{
-					label: __('Font Variation', 'blockera'),
+					label: __('Font Variation', 'blockera-pro'),
 					value: 'font-variation-settings',
 				},
 				{
-					label: __('Text Stroke Color', 'blockera'),
+					label: __('Text Stroke Color', 'blockera-pro'),
 					value: '-webkit-text-stroke-color',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Position Transitions', 'blockera'),
+			label: __('Position Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Top', 'blockera'),
+					label: __('Top', 'blockera-pro'),
 					value: 'top',
 				},
 				{
-					label: __('Right', 'blockera'),
+					label: __('Right', 'blockera-pro'),
 					value: 'right',
 				},
 				{
-					label: __('Bottom', 'blockera'),
+					label: __('Bottom', 'blockera-pro'),
 					value: 'bottom',
 				},
 				{
-					label: __('Left', 'blockera'),
+					label: __('Left', 'blockera-pro'),
 					value: 'left',
 				},
 				{
-					label: __('Z-Index', 'blockera'),
+					label: __('Z-Index', 'blockera-pro'),
 					value: 'z-index',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Margin Transitions', 'blockera'),
+			label: __('Margin Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Margin Top', 'blockera'),
+					label: __('Margin Top', 'blockera-pro'),
 					value: 'margin-top',
 				},
 				{
-					label: __('Margin Right', 'blockera'),
+					label: __('Margin Right', 'blockera-pro'),
 					value: 'margin-right',
 				},
 				{
-					label: __('Margin Bottom', 'blockera'),
+					label: __('Margin Bottom', 'blockera-pro'),
 					value: 'margin-bottom',
 				},
 				{
-					label: __('Margin Left', 'blockera'),
+					label: __('Margin Left', 'blockera-pro'),
 					value: 'margin-left',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Padding Transitions', 'blockera'),
+			label: __('Padding Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Padding Top', 'blockera'),
+					label: __('Padding Top', 'blockera-pro'),
 					value: 'padding-top',
 				},
 				{
-					label: __('Padding Right', 'blockera'),
+					label: __('Padding Right', 'blockera-pro'),
 					value: 'padding-right',
 				},
 				{
-					label: __('Padding Bottom', 'blockera'),
+					label: __('Padding Bottom', 'blockera-pro'),
 					value: 'padding-bottom',
 				},
 				{
-					label: __('Padding Left', 'blockera'),
+					label: __('Padding Left', 'blockera-pro'),
 					value: 'padding-left',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Flex Transitions', 'blockera'),
+			label: __('Flex Transitions', 'blockera-pro'),
 			options: [
 				{
-					label: __('Flex Grow', 'blockera'),
+					label: __('Flex Grow', 'blockera-pro'),
 					value: 'flex-grow',
 				},
 				{
-					label: __('Flex Shrink', 'blockera'),
+					label: __('Flex Shrink', 'blockera-pro'),
 					value: 'flex-shrink',
 				},
 				{
-					label: __('Flex Basis', 'blockera'),
+					label: __('Flex Basis', 'blockera-pro'),
 					value: 'flex-basis',
 				},
 			],
@@ -248,140 +248,140 @@ export const getTransitionTimingOptions = function (): Array<Object> {
 	return [
 		{
 			type: 'optgroup',
-			label: __('Default Timings', 'blockera'),
+			label: __('Default Timings', 'blockera-pro'),
 			options: [
 				{
-					label: __('Linear', 'blockera'),
+					label: __('Linear', 'blockera-pro'),
 					value: 'linear',
 				},
 				{
-					label: __('Ease', 'blockera'),
+					label: __('Ease', 'blockera-pro'),
 					value: 'ease',
 				},
 				{
-					label: __('Ease In', 'blockera'),
+					label: __('Ease In', 'blockera-pro'),
 					value: 'ease-in',
 				},
 				{
-					label: __('Ease Out', 'blockera'),
+					label: __('Ease Out', 'blockera-pro'),
 					value: 'ease-out',
 				},
 				{
-					label: __('Ease In Out', 'blockera'),
+					label: __('Ease In Out', 'blockera-pro'),
 					value: 'ease-in-out',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Ease In Timings', 'blockera'),
+			label: __('Ease In Timings', 'blockera-pro'),
 			options: [
 				{
-					label: __('Ease In Quad', 'blockera'),
+					label: __('Ease In Quad', 'blockera-pro'),
 					value: 'ease-in-quad',
 				},
 				{
-					label: __('Ease In Cubic', 'blockera'),
+					label: __('Ease In Cubic', 'blockera-pro'),
 					value: 'ease-in-cubic',
 				},
 				{
-					label: __('Ease In Quart', 'blockera'),
+					label: __('Ease In Quart', 'blockera-pro'),
 					value: 'ease-in-cubic',
 				},
 				{
-					label: __('Ease In Quint', 'blockera'),
+					label: __('Ease In Quint', 'blockera-pro'),
 					value: 'ease-in-quint',
 				},
 				{
-					label: __('Ease In Sine', 'blockera'),
+					label: __('Ease In Sine', 'blockera-pro'),
 					value: 'ease-in-sine',
 				},
 				{
-					label: __('Ease In Expo', 'blockera'),
+					label: __('Ease In Expo', 'blockera-pro'),
 					value: 'ease-in-expo',
 				},
 				{
-					label: __('Ease In Circ', 'blockera'),
+					label: __('Ease In Circ', 'blockera-pro'),
 					value: 'ease-in-circ',
 				},
 				{
-					label: __('Ease In Back', 'blockera'),
+					label: __('Ease In Back', 'blockera-pro'),
 					value: 'ease-in-back',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Ease Out Timings', 'blockera'),
+			label: __('Ease Out Timings', 'blockera-pro'),
 			options: [
 				{
-					label: __('Ease Out Quad', 'blockera'),
+					label: __('Ease Out Quad', 'blockera-pro'),
 					value: 'ease-out-quad',
 				},
 				{
-					label: __('Ease Out Cubic', 'blockera'),
+					label: __('Ease Out Cubic', 'blockera-pro'),
 					value: 'ease-out-cubic',
 				},
 				{
-					label: __('Ease Out Quart', 'blockera'),
+					label: __('Ease Out Quart', 'blockera-pro'),
 					value: 'ease-out-quart',
 				},
 				{
-					label: __('Ease Out Quint', 'blockera'),
+					label: __('Ease Out Quint', 'blockera-pro'),
 					value: 'ease-out-quint',
 				},
 				{
-					label: __('Ease Out Sine', 'blockera'),
+					label: __('Ease Out Sine', 'blockera-pro'),
 					value: 'ease-out-sine',
 				},
 				{
-					label: __('Ease Out Expo', 'blockera'),
+					label: __('Ease Out Expo', 'blockera-pro'),
 					value: 'ease-out-expo',
 				},
 				{
-					label: __('Ease Out Circ', 'blockera'),
+					label: __('Ease Out Circ', 'blockera-pro'),
 					value: 'ease-out-circ',
 				},
 				{
-					label: __('Ease Out Back', 'blockera'),
+					label: __('Ease Out Back', 'blockera-pro'),
 					value: 'ease-out-back',
 				},
 			],
 		},
 		{
 			type: 'optgroup',
-			label: __('Ease In Out Timings', 'blockera'),
+			label: __('Ease In Out Timings', 'blockera-pro'),
 			options: [
 				{
-					label: __('Ease In Out Quad', 'blockera'),
+					label: __('Ease In Out Quad', 'blockera-pro'),
 					value: 'ease-in-out-quad',
 				},
 				{
-					label: __('Ease In Out Cubic', 'blockera'),
+					label: __('Ease In Out Cubic', 'blockera-pro'),
 					value: 'ease-in-out-cubic',
 				},
 				{
-					label: __('Ease In Out Quart', 'blockera'),
+					label: __('Ease In Out Quart', 'blockera-pro'),
 					value: 'ease-in-out-quart',
 				},
 				{
-					label: __('Ease In Out Quint', 'blockera'),
+					label: __('Ease In Out Quint', 'blockera-pro'),
 					value: 'ease-in-out-quint',
 				},
 				{
-					label: __('Ease In Out Sine', 'blockera'),
+					label: __('Ease In Out Sine', 'blockera-pro'),
 					value: 'ease-in-out-sine',
 				},
 				{
-					label: __('easeInOutExpo', 'blockera'),
+					label: __('easeInOutExpo', 'blockera-pro'),
 					value: 'ease-in-out-expo',
 				},
 				{
-					label: __('Ease In Out Circ', 'blockera'),
+					label: __('Ease In Out Circ', 'blockera-pro'),
 					value: 'ease-in-out-circ',
 				},
 				{
-					label: __('Ease In Out Back', 'blockera'),
+					label: __('Ease In Out Back', 'blockera-pro'),
 					value: 'ease-in-out-back',
 				},
 			],
