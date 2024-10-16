849d3432cbf9c55e0bd9c1e9abbfc3d6b4bae799
rezaelahidev <rezaelahi.dev@gmail.com>
2024-10-16 16:32:26 +0330

    build: fix build-plugin-zip shell script

--- bin/build-plugin-zip.sh
+++ bin/build-plugin-zip.sh
@@ -103,7 +103,7 @@ status "Generating inc/app.php 📝"
 mkdir -p "inc"
 cp packages/blockera-pro/php/app.php inc/app.php
 
-build_files=$(find dist/ -type f \( -name "*.min.js" -o -name "*.min.css" -o -name "asset.php" \))
+build_files=$(find dist/ -type f \( -name "*.min.js" -o -name "*.min.css" -o -name "*.min.asset.php" \))
 
 if [ -z "$build_files" ]; then
     warning "No minified CSS, JS, or asset.php files found in the dist directory. 🤔"
