d329eeccf93129c3d65abc6f292617d3dd233884
rezaelahidev <rezaelahi.dev@gmail.com>
2024-11-11 12:19:00 +0330

    feat: apply block states costomization

--- packages/editor-pro/js/extensions/libs/block-states/index.js
+++ packages/editor-pro/js/extensions/libs/block-states/index.js
@@ -5,7 +5,7 @@
  */
 import { addFilter } from '@wordpress/hooks';
 
-export const applyBlockStates = (): void =>
+export const applyBlockStates = (): void => {
 	addFilter(
 		`blockera.controls.block-states.props`,
 		'blockera.pro.controls.block-states.props',
@@ -16,3 +16,40 @@ export const applyBlockStates = (): void =>
 			};
 		}
 	);
+
+	addFilter(
+		'blockera.editor.styleEngine.allowedStates',
+		'blockera.pro.editor.styleEngine.customization',
+		(allowedStates: Array<string>): Array<string> => [
+			...allowedStates,
+			'active',
+			'focus',
+			'visited',
+			'before',
+			'after',
+			'custom-class',
+			'parent-class',
+			'parent-hover',
+		]
+	);
+};
+
+export const applyDefaultBlockStates = (): void => {
+	addFilter(
+		'blockera.editor.extensions.blockStates.availableStates',
+		'blockera.pro.controls.block-states.items',
+		(states: Object): Object => {
+			for (const stateKey in states) {
+				const state = states[stateKey];
+
+				if (state?.disabled) {
+					state.disabled = false;
+				}
+
+				state.label = state.label.replace(' - Upgrade to RPO', '');
+			}
+
+			return states;
+		}
+	);
+};
