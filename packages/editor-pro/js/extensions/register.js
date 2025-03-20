d4f42e4a3626a13ca8628eaee2b795ea7772e8fb
rezaelahidev <rezaelahi.dev@gmail.com>
2025-03-21 01:38:53 +0330

    feat: protected the apply pro features in registration editor extensions 🔒

--- packages/editor-pro/js/extensions/register.js
+++ packages/editor-pro/js/extensions/register.js
@@ -9,6 +9,7 @@ import { addFilter } from '@wordpress/hooks';
  * Blockera dependencies
  */
 import { mergeObject } from '@blockera/utils';
+import { validateSecretKeys } from '@blockera/validator';
 
 /**
  * Internal dependencies
@@ -17,6 +18,91 @@ import * as config from './config';
 import { applyBlockStates } from './libs';
 
 export const registerEditorExtensions = () => {
+	const { blockeraAccount: account } = window;
+	const {
+		client_id: clientId,
+		client_secret: clientSecret,
+		access_token: accessToken,
+		refresh_token: refreshToken,
+		license: {
+			id,
+			name,
+			status,
+			startDate,
+			licenseKey,
+			nextPaymentDueDate,
+		},
+	} = account || {
+		license: {},
+	};
+	const domain = window.location.origin;
+	const subscriptionId = id;
+
+	if (
+		!id ||
+		!accessToken ||
+		!refreshToken ||
+		!status ||
+		!name ||
+		!licenseKey ||
+		!nextPaymentDueDate ||
+		!startDate ||
+		!clientId ||
+		!clientSecret
+	) {
+		console.warn(
+			'Invalid registered license! please check your domain and license in the https://blockera.ai'
+		);
+		return;
+	}
+
+	if ('active' !== status) {
+		console.warn(
+			'Your license is not active! please check your domain and license in the https://blockera.ai'
+		);
+		return;
+	}
+
+	// Start Validation: Secret keys.
+	const validated = validateSecretKeys({
+		domain,
+		clientId,
+		clientSecret,
+		licenseKey,
+		subscriptionId,
+	});
+
+	if (!validated) {
+		console.warn(
+			'Invalid registered license! please check your domain and license in the https://blockera.ai'
+		);
+		return;
+	}
+
+	// Validation: Subscription name.
+	if (-1 === name.startsWith(`#${id} - `)) {
+		console.warn(
+			'Invalid registered license! please check your domain and license in the https://blockera.ai'
+		);
+		return;
+	}
+
+	// Validation: Next payment due date.
+	if (new Date(nextPaymentDueDate) < new Date()) {
+		console.warn(
+			'Your license is expired! please check your domain and license in the https://blockera.ai'
+		);
+		return;
+	}
+
+	// Validation: Start date.
+	if (new Date(startDate) > new Date()) {
+		console.warn(
+			'Your license is not started! it seems that your license invalid or ex please check your domain and license in the https://blockera.ai'
+		);
+		return;
+	}
+
 	addFilter(
 		'blocks.registerBlockType',
 		'blockeraPro-editorExtensions',
