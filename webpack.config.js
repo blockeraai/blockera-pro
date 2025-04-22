044ae353f58961ae540a6f6f0597248acf331bbb
rezaelahidev <rezaelahi.dev@gmail.com>
2025-04-22 19:45:13 +0330

    chore: modified the build process and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = (env, argv) => {
 
 			return packageName;
 		});
-	const blockeraPackagesVersion = Object.fromEntries(
+	let blockeraPackagesVersion = Object.fromEntries(
 		blockeraPackages.map((packageName) => {
 			let parentDirectory = '';
 			let name = packageName;
@@ -74,6 +74,22 @@ module.exports = (env, argv) => {
 			return [packageName, version.replace(/\./g, '_')];
 		})
 	);
+	// FIXME: please update the version of the packages to the latest version with the specific workflow in continuous integration.
+	blockeraPackagesVersion = {
+		...blockeraPackagesVersion,
+		editor: '1_3_0',
+		icons: '1_0_0',
+		storage: '1_0_0',
+		telemetry: '1_0_2',
+		env: '1_0_0',
+		bootstrap: '1_0_1',
+		wordpress: '1_1_2',
+		classnames: '1_0_0',
+		dataEditor: '1_0_0',
+		blocksCore: '1_2_0',
+		controls: '1_2_0',
+		data: '1_1_0',
+	};
 	const blockeraEntries = blockeraPackages.reduce((memo, packageName) => {
 		// Exclude dev packages.
 		if (-1 !== packageName.indexOf('dev-')) {
@@ -157,6 +173,8 @@ module.exports = (env, argv) => {
 				'blockeraClassnames_' + blockeraPackagesVersion.classnames,
 			'@blockera/data-editor':
 				'blockeraDataEditor_' + blockeraPackagesVersion['data-editor'],
+			'@blockera/feature-manager':
+				'blockeraFeatureManager_' + blockeraPackagesVersion.guard,
 		},
 	});
 };
