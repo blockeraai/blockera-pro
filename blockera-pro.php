0eed80f4bddbe84d89b104bfb4939b1aad7875db
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-22 10:50:16 +0330

    chore: bump to v1.0.0 - RC 2

--- blockera-pro.php
+++ blockera-pro.php
@@ -8,7 +8,7 @@
  * Requires PHP: 7.4
  * Author: Blockera AI
  * Author URI: https://blockera.ai/about/
- * Version: 1.0.0-rc.1
+ * Version: 1.0.0-rc.2
  * Text Domain: blockera-pro
  * License: GPLv3 or later
  *
