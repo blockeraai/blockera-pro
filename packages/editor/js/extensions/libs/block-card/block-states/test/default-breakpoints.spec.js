a23f8fe2cdb7a932082ff8431aa504dd342954f2
blockerabot <blockeraai+githubbot@gmail.com>
2025-05-29 14:09:13 +0000

    Sync shared packages from blockera

---
+++ packages/editor/js/extensions/libs/block-card/block-states/test/default-breakpoints.spec.js
@@ -0,0 +1,17 @@
+/**
+ * Internal dependencies
+ */
+import { default as defaultBreakpoints } from '../default-breakpoints';
+
+describe('Default breakpoints settings', () => {
+	describe('Base breakpoint', () => {
+		it('base breakpoint is desktop', () => {
+			expect(defaultBreakpoints()['desktop'].base).toBe(true);
+		});
+
+		it('base breakpoint should not have min and max', () => {
+			expect(defaultBreakpoints()['desktop'].settings.min).toBe('');
+			expect(defaultBreakpoints()['desktop'].settings.max).toBe('');
+		});
+	});
+});
