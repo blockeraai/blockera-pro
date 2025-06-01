eae6c23b8076464e05c11af0761a2b220b127054
rezaelahidev <rezaelahi.dev@gmail.com>
2025-06-01 20:49:41 +0330

    compat: with the php@7.4

--- packages/editor-pro/php/StyleEngine.php
+++ packages/editor-pro/php/StyleEngine.php
@@ -345,7 +345,7 @@ final class StyleEngine {
 		// Attempt to create new instance.
 		try {
 			$this->definition = $this->app->make($definition, [ 'supports' => $this->supports ]);
-		} catch (\Exception) {
+		} catch (\Exception $e) {
 			return;
 		}
 	}
