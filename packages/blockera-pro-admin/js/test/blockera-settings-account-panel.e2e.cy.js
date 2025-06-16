9e78ef63516d2c399a6b1bad8b3d227274c05235
rezaelahidev <rezaelahi.dev@gmail.com>
2025-06-16 18:11:52 +0330

    test: moved to other location

--- packages/blockera-pro-admin/js/test/blockera-settings-account-panel.e2e.cy.js
+++ packages/blockera-pro-admin/js/test/blockera-settings-account-panel.e2e.cy.js
@@ -1,43 +1,28 @@
-import {
-	createPost,
-	goTo,
-} from '@blockera/dev-cypress/js/helpers/site-navigation';
-import {
-	appendBlocks,
-	resetPanelSettings,
-} from '@blockera/dev-cypress/js/helpers';
+import { goTo } from '@blockera/dev-cypress/js/helpers';
 
-describe('Blockera Settings Account Panel Testing ...', () => {
-	it('should redirect to the activate pro license page after activating the plugin', () => {
-		goTo('/wp-admin/plugins.php').then(() => {
-			cy.getByAriaLabel('Deactivate Blockera PRO').click();
-			cy.getByAriaLabel('Activate Blockera PRO').click();
+describe('Activate License', () => {
+	it('should activate license', () => {
+		goTo('/wp-admin/admin.php?page=blockera-settings-account');
+		cy.getByDataTest('activate-license-button').click();
 
-			cy.get('button').contains('Activate License').should('be.visible');
-		});
-	});
+		cy.url().then((url) => {
+			if (url.includes('/wp-login.php')) {
+				cy.get('#user_login').type(Cypress.env('wpUsername'), {
+					delay: 0,
+				});
+				cy.get('#user_pass').type(Cypress.env('wpPassword'), {
+					delay: 0,
+				});
+				cy.get('#wp-submit').click();
 
-	it('should render the Activate Pro License menu at the dashboard while user not logged in', () => {
-		goTo('/wp-admin/admin.php?page=blockera-settings-account').then(() => {
-			if (Cypress.$('#skip_activation').length) {
-				cy.get('#skip_activation').click();
+				cy.getByDataTest('connect-button').click();
+				cy.getByDataTest('create-page-button').should('be.visible');
+				cy.getByDataTest('manage-licenses-button').should('be.visible');
+
+				cy.getByDataTest('manage-licenses-button').click();
+
+				cy.getByDataTest('account-info').should('be.visible');
 			}
-
-			cy.get('button').contains('Activate License').should('be.visible');
-
-			cy.get('li.blockera-pro-submenu')
-				.contains('Activate Pro License')
-				.should('be.visible');
-		});
-	});
-
-	it('should user can activate pro license while click on Activate License button', () => {
-		goTo('/wp-admin/admin.php?page=blockera-settings-account').then(() => {
-			if (Cypress.$('#skip_activation').length) {
-				cy.get('#skip_activation').click();
-			}
-
-			cy.get('button').contains('Activate License').click();
 		});
 	});
 });
