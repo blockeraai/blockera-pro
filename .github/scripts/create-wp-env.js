5677ab798fb99bdf6568f579730687e70642c103
rezaelahidev <rezaelahi.dev@gmail.com>
2025-06-21 14:38:55 +0330

    chore: improved the .wp-env.json creation for multiple scenarios

--- .github/scripts/create-wp-env.js
+++ .github/scripts/create-wp-env.js
@@ -3,45 +3,20 @@ const fs = require('fs');
 
 const [category] = process.argv.slice(2);
 
-// Read the .pr-env.json file
+// Read the .pr-env.json file.
 const prEnv = JSON.parse(fs.readFileSync('.pr-env.json', 'utf-8'));
+// Read the config from wp-env-configs files.
+const wpEnvConfig = fs.readFileSync(
+	'.github/wp-env-configs/' + category + '.json',
+	'utf-8'
+);
 
-// Merge the .plugins property with static plugins
-let plugins;
-
-switch (category) {
-	case 'woocommerce':
-		plugins = [
-			...(prEnv.plugins || []),
-			...[
-				'https://downloads.wordpress.org/plugin/woocommerce.latest-stable.zip',
-			],
-		];
-		break;
-	case 'freemius':
-		plugins = prEnv.plugins;
-		break;
-	case 'plugins':
-		plugins = [
-			...(prEnv.plugins || []),
-			...[
-				'https://downloads.wordpress.org/plugin/icon-block.latest-stable.zip',
-			],
-		];
-		break;
-	default:
-		plugins = [
-			...(prEnv.plugins || []),
-			...[
-				'https://downloads.wordpress.org/plugin/svg-support.latest-stable.zip',
-			],
-		];
-		break;
-}
-
-// Create the .wp-env.json content
+// Create the .wp-env.json content.
 const wpEnvContent = {
-	plugins,
+	...(wpEnvConfig.hasOwnProperty('themes')
+		? { themes: wpEnvConfig.themes }
+		: {}),
+	plugins: [...(wpEnvConfig?.plugins || []), ...(prEnv?.plugins || [])],
 	config: {
 		WP_DEBUG: false,
 		SCRIPT_DEBUG: false,
