f651fafb3423e78a1dbe7176584ef9a19f731290
rezaelahidev <rezaelahi.dev@gmail.com>
2025-06-21 15:19:52 +0330

    chore: fixed the create-wp-env.js script issues

--- .github/scripts/create-wp-env.js
+++ .github/scripts/create-wp-env.js
@@ -6,9 +6,8 @@ const [category] = process.argv.slice(2);
 // Read the .pr-env.json file.
 const prEnv = JSON.parse(fs.readFileSync('.pr-env.json', 'utf-8'));
 // Read the config from wp-env-configs files.
-const wpEnvConfig = fs.readFileSync(
-	'.github/wp-env-configs/' + category + '.json',
-	'utf-8'
+const wpEnvConfig = JSON.parse(
+	fs.readFileSync('.github/wp-env-configs/' + category + '.json', 'utf-8')
 );
 
 // Create the .wp-env.json content.
@@ -16,7 +15,12 @@ const wpEnvContent = {
 	...(wpEnvConfig.hasOwnProperty('themes')
 		? { themes: wpEnvConfig.themes }
 		: {}),
-	plugins: [...(wpEnvConfig?.plugins || []), ...(prEnv?.plugins || [])],
+	plugins: [
+		...new Set([
+			...(wpEnvConfig?.plugins || []),
+			...(prEnv?.plugins || []),
+		]),
+	],
 	config: {
 		WP_DEBUG: false,
 		SCRIPT_DEBUG: false,
@@ -29,8 +33,4 @@ fs.writeFileSync(
 	'.wp-env.json',
 	JSON.stringify(wpEnvContent, null, 2),
 	'utf-8'
-);
-
-console.log(
-	'Merged plugins between .pr-env.json file and required plugins of matrix category and written to .wp-env.json'
 );
