2424842bf0c19c9814875765a1bfa5cb33665c61
rezaelahidev <rezaelahi.dev@gmail.com>
2025-06-23 10:05:49 +0330

    test: improved tests for the activating license process

--- packages/blockera-pro-admin/js/test/blockera-settings-account.panels.e2e.cy.js
+++ packages/blockera-pro-admin/js/test/blockera-settings-account.panels.e2e.cy.js
@@ -7,13 +7,18 @@ describe('Activate License', () => {
 
 		cy.url().then((url) => {
 			if (url.includes('/wp-login.php')) {
-				cy.get('#user_login').type(Cypress.env('blockeraUserName'), {
-					delay: 0,
-				});
-				cy.get('#user_pass').type(Cypress.env('blockeraPassword'), {
-					delay: 0,
-				});
+				cy.get('#user_login').type(Cypress.env('blockeraUserName'));
+				cy.get('#user_pass').type(Cypress.env('blockeraPassword'));
 				cy.get('#wp-submit').click();
+
+				cy.get('input')
+					.eq(0)
+					.parent()
+					.then(($parent) => {
+						if (!$parent.hasClass('is-checked')) {
+							cy.get('input').eq(0).click();
+						}
+					});
 
 				cy.getByDataTest('connect-button').click();
 				cy.getByDataTest('create-page-button').should('be.visible');
@@ -22,6 +27,79 @@ describe('Activate License', () => {
 				cy.getByDataTest('manage-licenses-button').click();
 
 				cy.getByDataTest('account-info').should('be.visible');
+
+				// Goto BlockeraAi website license panel for blockerabot account.
+				cy.visit('https://blockera.ai/my-account/licenses');
+
+				cy.get('input#username').type(Cypress.env('blockeraUserName'));
+				cy.get('input#password').type(Cypress.env('blockeraPassword'));
+				cy.get('button[type="submit"]').click();
+
+				cy.getByDataTest('website-url')
+					.eq(0)
+					.contains(
+						Cypress.env('testURL').replace(/https?:\/\//, '')
+					);
+			}
+		});
+	});
+
+	it('should clear registered licenses and try again to login and activate license', () => {
+		goTo('/wp-admin/admin.php?page=blockera-settings-account');
+
+		cy.request(
+			'POST',
+			Cypress.env('testURL') + '/wp-json/blockera/v1/auth/clear-licenses',
+			{
+				email: 'blockeraai+githubbot@gmail.com',
+			}
+		)
+			.its('status')
+			.should('eq', 200);
+
+		cy.reload();
+
+		cy.getByDataTest('activate-license-button').click();
+
+		cy.url().then((url) => {
+			if (url.includes('/wp-login.php')) {
+				cy.get('#user_login').type(Cypress.env('blockeraUserName'), {
+					delay: 0,
+				});
+				cy.get('#user_pass').type(Cypress.env('blockeraPassword'), {
+					delay: 0,
+				});
+				cy.get('#wp-submit').click();
+
+				cy.get('input')
+					.eq(0)
+					.parent()
+					.then(($parent) => {
+						if (!$parent.hasClass('is-checked')) {
+							cy.get('input').eq(0).click();
+						}
+					});
+
+				cy.getByDataTest('connect-button').click();
+				cy.getByDataTest('create-page-button').should('be.visible');
+				cy.getByDataTest('manage-licenses-button').should('be.visible');
+
+				cy.getByDataTest('manage-licenses-button').click();
+
+				cy.getByDataTest('account-info').should('be.visible');
+
+				// Goto BlockeraAi website license panel for blockerabot account.
+				cy.visit('https://blockera.ai/my-account/licenses');
+
+				cy.get('input#username').type(Cypress.env('blockeraUserName'));
+				cy.get('input#password').type(Cypress.env('blockeraPassword'));
+				cy.get('button[type="submit"]').click();
+
+				cy.getByDataTest('website-url')
+					.eq(0)
+					.contains(
+						Cypress.env('testURL').replace(/https?:\/\//, '')
+					);
 			}
 		});
 	});
