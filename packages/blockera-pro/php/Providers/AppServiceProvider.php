1bf84329049f939a3de1c6204cb4b229e3eb2418
rezaelahidev <rezaelahi.dev@gmail.com>
2025-07-06 19:04:18 +0330

    fix: base breakpoint accessibility

--- packages/blockera-pro/php/Providers/AppServiceProvider.php
+++ packages/blockera-pro/php/Providers/AppServiceProvider.php
@@ -45,7 +45,7 @@ class AppServiceProvider extends ServiceProvider {
 					$style_engine = new SiteBuilderStyleEngine($params['block'], $params['fallbackSelector']);
 
 					$style_engine->setApp($blockera);
-					$style_engine->setBreakpoint(blockera_pro_core_config('breakpoints.base'));
+					$style_engine->setBreakpoint(blockera_core_config('breakpoints.base'));
 					$style_engine->setBreakpoints($app->getEntity('breakpoints'));
 
 					return $style_engine;
