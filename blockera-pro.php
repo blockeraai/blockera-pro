c1f440c73690ad27d17a3e075d2a6ebe1b2c20c6
rezaelahidev <rezaelahi.dev@gmail.com>
2025-08-12 13:47:04 +0330

    fix: php warning while  is empty

--- blockera-pro.php
+++ blockera-pro.php
@@ -86,7 +86,7 @@ add_action('plugins_loaded', 'blockera_pro_init', 5);
  */
 function blockera_pro_init(): void {
 
-	$env_mode = 'development' === $_ENV['APP_MODE'] ?? 'production';
+	$env_mode = 'development' === ( $_ENV['APP_MODE'] ?? 'production' );
 	$mode     = defined('BLOCKERA_PRO_APP_MODE') && 'development' === BLOCKERA_PRO_APP_MODE && $env_mode;
 
 	global $blockera_compat_pro_with_free;
