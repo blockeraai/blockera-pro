3fffb0d268890070edf74553ad267b616743ba0c
blockerabot <blockeraai+githubbot@gmail.com>
2025-08-13 09:45:59 +0000

    Sync shared packages from blockera

--- packages/icons/js/library-fasolid/icons.js
+++ packages/icons/js/library-fasolid/icons.js
@@ -21,60 +21,16 @@ const FaSolidIcons: Object = Object.fromEntries(
 			let newKey = getIconKebabId(key);
 
 			switch (newKey) {
-				case 'fa-5-0-0px':
-					newKey = '500px';
-					break;
-
 				case 'fa-dice-d-2-0':
-					newKey = 'dice-d20';
+					newKey = 'fa-dice-d20';
 					break;
 
 				case 'fa-dice-d-6':
-					newKey = 'dice-d6';
+					newKey = 'fa-dice-d6';
 					break;
 
 				case 'fa-stopwatch-2-0':
-					newKey = 'stopwatch-20';
-					break;
-
-				case 'fa-1-1ty':
-					newKey = '11ty';
-					break;
-
-				case 'fa-4-2-group':
-					newKey = '42-group';
-					break;
-
-				case 'fa-css-3':
-					newKey = 'css3';
-					break;
-
-				case 'fa-css-3-alt':
-					newKey = 'css3-alt';
-					break;
-
-				case 'fa-html-5':
-					newKey = 'html5';
-					break;
-
-				case 'fa-draft-2digital':
-					newKey = 'draft2digital';
-					break;
-
-				case 'fa-ns-8':
-					newKey = 'ns8';
-					break;
-
-				case 'fa-page-4':
-					newKey = 'page4';
-					break;
-
-				case 'fa-typo-3':
-					newKey = 'typo3';
-					break;
-
-				case 'fa-w-3c':
-					newKey = 'w3c';
+					newKey = 'fa-stopwatch-20';
 					break;
 
 				default:
@@ -84,7 +40,7 @@ const FaSolidIcons: Object = Object.fromEntries(
 			return [newKey, value];
 		})
 		.filter(([key]) => {
-			return !['fas'].includes(key);
+			return !['fas', 'prefix'].includes(key) && !key.endsWith('-alt');
 		})
 );
 
