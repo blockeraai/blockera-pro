0e604f8eb9b050d1fb04b25c80173f98abc53afd
rezaelahidev <rezaelahi.dev@gmail.com>
2025-08-31 22:41:59 +0330

    test(plugin-compatibility): added the new test suite

--- packages/plugin-compatibility-pro/js/tests/blockera-pro-check.plugin-compatibility.e2e.cy.js
+++ packages/plugin-compatibility-pro/js/tests/blockera-pro-check.plugin-compatibility.e2e.cy.js
@@ -1,4 +1,11 @@
-import { goTo } from '@blockera/dev-cypress/js/helpers/site-navigation';
+import {
+	goTo,
+	savePage,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+	createPost,
+} from '@blockera/dev-cypress/js/helpers';
 
 describe('Blockera PRO plugin compatibility checks', () => {
 	it('should be able to see plugin compatibility page while user try to navigate WordPress admin pages if not compatible with free version', () => {
@@ -15,5 +22,53 @@ describe('Blockera PRO plugin compatibility checks', () => {
 		goTo('/wp-admin/site-editor.php');
 
 		cy.contains('Update Required for Blockera').should('be.visible');
+
+		goTo('/wp-admin/');
+
+		cy.contains('Update Required for Blockera').should('be.visible');
+	});
+
+	it('should not conflicted with free plugin default functionality like style engine and render modules', () => {
+		createPost();
+
+		cy.getBlock('default').type('This is test paragraph', { delay: 0 });
+		cy.get('[aria-label="Settings"]').eq(1).click({ force: true });
+		cy.getByDataTest('style-tab').click();
+
+		// add alias to the feature container
+		cy.getParentContainer('Clipping').as('clippingContainer');
+
+		cy.get('@clippingContainer').within(() => {
+			// act: clicking on clipping button
+			cy.get('button').as('clippingBtn');
+			cy.get('@clippingBtn').click();
+
+			// select corresponding option
+			cy.contains('div', 'Clip to Padding').click();
+		});
+
+		//assert data
+		getWPDataObject().then((data) => {
+			expect(
+				getSelectedBlock(data, 'blockeraBackgroundClip')
+			).to.be.equal('padding-box');
+		});
+
+		//assert block
+		cy.getBlock('core/paragraph').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//assert  frontend
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.blockera-block').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
 	});
 });
