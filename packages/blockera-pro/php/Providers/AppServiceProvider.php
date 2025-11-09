ddf84010054766845e394bbadb740c80015f5760
Ali Aghdam <betteraliaghdam@gmail.com>
2025-11-09 17:52:53 +0330

    feat(Columns Count): move css generator to Free

--- packages/blockera-pro/php/Providers/AppServiceProvider.php
+++ packages/blockera-pro/php/Providers/AppServiceProvider.php
@@ -174,7 +174,6 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function registerSiteBuilderStyleEngine( Application $app): void {
         $styleDefinitions = [
-            'ColumnCount' => \Blockera\SiteBuilder\StyleDefinitions\ColumnCount::class,
             'Content' => \Blockera\SiteBuilder\StyleDefinitions\Content::class,
             'Filter' => \Blockera\SiteBuilder\StyleDefinitions\Filter::class,
 			'Flex' => \Blockera\SiteBuilder\StyleDefinitions\Flex::class,
