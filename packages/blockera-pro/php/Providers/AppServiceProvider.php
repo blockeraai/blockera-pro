f46207e81131b5bcc3006b6de0c0bcd5c38d5729
Ali Aghdam <betteraliaghdam@gmail.com>
2025-11-09 17:58:28 +0330

    feat(Content): move css generator to Free

--- packages/blockera-pro/php/Providers/AppServiceProvider.php
+++ packages/blockera-pro/php/Providers/AppServiceProvider.php
@@ -174,7 +174,6 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function registerSiteBuilderStyleEngine( Application $app): void {
         $styleDefinitions = [
-            'Content' => \Blockera\SiteBuilder\StyleDefinitions\Content::class,
             'Filter' => \Blockera\SiteBuilder\StyleDefinitions\Filter::class,
 			'Flex' => \Blockera\SiteBuilder\StyleDefinitions\Flex::class,
             'Mouse' => \Blockera\SiteBuilder\StyleDefinitions\Mouse::class,
