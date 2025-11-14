c66d4c998002c13767d3f2f6ac5dd66d82386a53
blockerabot <blockeraai+githubbot@gmail.com>
2025-11-14 14:24:59 +0000

    Sync shared packages from blockera

--- packages/blocks-core/php/libs/wordpress/embed/block.php
+++ packages/blocks-core/php/libs/wordpress/embed/block.php
@@ -4,7 +4,7 @@
  *
  * @var array $args the block arguments!
  *
- * @package blockera/packages/blocks/js/wordpress/file
+ * @package blockera/packages/blocks/js/wordpress/embed
  */
 
 return array_merge(
@@ -12,12 +12,10 @@ return array_merge(
 	[
 		'selectors' => array_merge(
 			$args['selectors'] ?? [],
-			(array) array_merge(
-				blockera_load( 'inners.link', dirname( __DIR__ ) ),
-			),
 			[
-				'blockera/core/button' => [
-					'root' => '.wp-element-button',
+				'root' => ' .wp-block-embed__wrapper',
+				'blockera/elements/caption' => [
+					'root' => '&& figcaption',
 				],
 			]
 		),
