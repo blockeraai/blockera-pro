c471abfcd414bd3797d5b749fab01cd3b1ca464b
blockerabot <blockeraai+githubbot@gmail.com>
2025-11-29 21:38:15 +0000

    Sync shared packages from blockera

--- packages/wordpress/php/RenderBlock/Traits/Processor.php
+++ packages/wordpress/php/RenderBlock/Traits/Processor.php
@@ -5,6 +5,20 @@ namespace Blockera\WordPress\RenderBlock\Traits;
 trait Processor {
 
 	/**
+	 * Store the is doing transpiling flag property.
+	 *
+	 * @var bool $is_doing_transpile 
+	 */
+	protected bool $is_doing_transpile = false;
+
+	/**
+	 * The classname for the transpiled block.
+	 *
+	 * @var string
+	 */
+	protected string $transpile_classname = 'be-transpiled';
+
+	/**
      * Store styles.
      *
      * @var array
@@ -12,98 +26,47 @@ trait Processor {
     protected array $styles = [];
 
 	/**
-	 * Global css props classes.
+	 * Store inline styles collected from the block html.
 	 *
-	 * @var array
+	 * @var array $inline_styles the inline styles array.
 	 */
-	protected array $global_css_props_classes = [];
+	protected array $inline_styles = [];
 
 	/**
-	 * Set the global css props classes.
+	 * Normalize inline styles.
 	 *
-	 * @param array $global_css_props_classes The global css props classes.
+	 * @param string $classname the block classname.
 	 *
-	 * @return void
+	 * @return array the normalized inline styles.
 	 */
-	public function setGlobalCssPropsClasses( array $global_css_props_classes): void {
-		$this->global_css_props_classes = $global_css_props_classes;
-	}
+	protected function normalizeInlineStyles( string $classname): array {
+		$inline_styles = [];
 
-	/**
-     * Update classname for current tag.
-     *
-     * @param \WP_HTML_Tag_Processor $processor The HTML tag processor object.
-     * @param string                 $classname The classname to update.
-	 * @param array                  $block The block data.
-     *
-     * @return void
-     */
-    protected function updateClassname( \WP_HTML_Tag_Processor $processor, string $classname, array $block): void {
-        $previous_class  = $processor->get_attribute('class');
-        $regexp          = blockera_get_unique_class_name_regex();
-		$final_classname = '';
+		if (! empty($this->inline_styles) && $this->is_doing_transpile) {
 
-        if (! empty($previous_class) && ! empty($classname)) {
+			foreach ($this->inline_styles as $_selector => $declarations) {
 
-            if (preg_match($regexp, $classname, $matches) && ! preg_match($regexp, $previous_class)) {
-
-                $final_classname = $classname . ' ' . $previous_class;
-            } else {
-
-				if (! preg_match($regexp, $classname) && ! str_contains($previous_class, $classname)) {
-
-					$final_classname = $classname . ' ' . $previous_class;
-				} else {
-
-					if (! str_contains($classname, 'be-transpiled') && preg_match($regexp, $previous_class) && preg_match($regexp, $classname)) {
-
-						$classname = preg_replace('/\./', ' ', $classname);
-
-						if (str_contains($previous_class, $classname)) {
-
-							if (! str_contains($previous_class, 'be-transpiled') && ! blockera_block_has_icon($block)) {
-								$previous_class = str_replace($classname, $classname . ' be-transpiled', $previous_class);
-							}
-						}
+				foreach ($declarations as $declaration) {
+					if (empty(trim($declaration))) {
+						continue;
 					}
 
-					$final_classname = $previous_class;
+					// Normalizing declaration.
+					$declaration = preg_replace('/\:/', ': ', trim($declaration));
+
+					// handle root element inline styles.
+					if ($_selector === $classname) {
+						$inline_styles['root'][ $classname ][] = $declaration;
+						continue;
+					}
+
+					// handle child elements inline styles.
+					$inline_styles['child'][ $_selector ][] = $declaration;
 				}
 			}
-        }
-
-		// Prevent double adding the be-transpiled class to block wrapper element.
-		// It should has not icon element.
-		if (! empty($final_classname) && ! str_contains($final_classname, 'be-transpiled') && ! blockera_block_has_icon($block)) {
-			$final_classname .= ' be-transpiled';
 		}
 
-		if (! empty($final_classname)) {
-			$processor->set_attribute('class', $final_classname);
-		}
-    }
-
-	/**
-	 * Add css props classes to the classname of current tag.
-	 * Update classname based on global css props classes.
-	 * Just for backward compatibility with WordPress original block output.
-	 *
-	 * @param \WP_HTML_Tag_Processor $processor The HTML tag processor object.
-	 * @param string                 $style The style attribute value.
-	 * @param array                  $block The block data.
-	 *
-	 * @return void
-	 */
-	protected function addCssPropsClasses( \WP_HTML_Tag_Processor $processor, string $style, array $block): void {
-		if (! $style) {
-			return;
-		}
-
-		foreach ($this->global_css_props_classes as $prop => $prop_class) {
-			if (str_contains($style, $prop)) {
-				$this->updateClassname($processor, $prop_class, $block);
-			}
-		}
+		return $inline_styles;
 	}
 
 	/**
@@ -123,7 +86,7 @@ trait Processor {
 		$classes = array_filter(
             $classes,
             function ( $class) {
-				return 'be-transpiled' !== $class;
+				return $this->transpile_classname !== $class;
 			}
         );
 
@@ -172,37 +135,18 @@ trait Processor {
 			return;
 		}
 
-		foreach ($inline_styles as $root_selector => $styles) {
-			$inners = array_filter(
-				$styles,
-				function ( $style) {
-					return is_array($style);
-				}
-			);
+		foreach ($inline_styles as $selector => $styles) {
+			$css = implode(';' . PHP_EOL, $styles);
 
-			foreach ($inners as $selector => $declarations) {
-				// Normalize declarations.
-				$declarations = preg_replace('/\:/', ': ', $declarations);
-				// Create css rule.
-				$css_rule = $selector . ' { ' . implode(';' . PHP_EOL, $declarations) . ' }';
-
-				if (! in_array($css_rule, $this->styles, true)) {
-					$this->styles[] = $css_rule;
-				}
-
-				unset($styles[ $selector ]);
+			if (! str_ends_with($css, ';')) {
+				$css .= ';';
 			}
 
-			// Ensure that the root style is added to the styles property while $styles is not empty.
-			if (! empty($styles)) {
-				// Normalize styles.
-				$styles = preg_replace('/\:/', ': ', $styles);
-				// Create css rule.
-				$css_rule = $root_selector . ' { ' . implode(';' . PHP_EOL, $styles) . ' }';
+			// Create css rule.
+			$css_rule = $selector . ' { ' . $css . ' }';
 
-				if (! in_array($css_rule, $this->styles, true)) {
-					$this->styles[] = $css_rule;	
-				}
+			if (! in_array($css_rule, $this->styles, true)) {
+				$this->styles[] = $css_rule;
 			}
 		}
 	}
