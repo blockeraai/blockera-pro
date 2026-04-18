ba3d00bb772b9a2ae16d3bab90ba7f7b2ef705d7
blockerabot <blockeraai+githubbot@gmail.com>
2026-04-18 06:34:44 +0000

    Sync shared packages from blockera

--- packages/global-styles-ui/js/colors/color-palette-screen.tsx
+++ packages/global-styles-ui/js/colors/color-palette-screen.tsx
@@ -108,6 +108,7 @@ function ColorGroupComponent({
 
 	const defaultPresetValue = useMemo(
 		() => ({
+			isVisible: true,
 			color: '#000000',
 			slug: `custom-${index}`,
 			deletable: origin === 'custom',
@@ -286,6 +287,7 @@ function ColorPaletteScreen({ onBackHandler }: ColorPaletteScreenProps) {
 		<VStack spacing={2} className="blockera-color-palette-presets">
 			<ScreenHeader
 				onBack={onBackHandler}
+				dataTest="global-styles-color-palette-screen"
 				title={__('Color Variables', 'blockera')}
 				description={__(
 					'Create and edit color variables used for text, backgrounds, and borders.',
