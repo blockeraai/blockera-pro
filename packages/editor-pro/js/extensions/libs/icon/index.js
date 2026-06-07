ce45fcd0f9fe8b62d6a13f19b766afc9afeed0f8
Ali Aghdam <betteraliaghdam@gmail.com>
2026-06-07 16:19:36 +0330

    fix(editor-pro): preserve custom SVG uploads without feature-icon global
    Bundle custom SVG encode helpers locally so editor-pro does not depend on
    the feature-icon webpack global, which loads after blockera-pro in assets.

--- packages/editor-pro/js/extensions/libs/icon/index.js
+++ packages/editor-pro/js/extensions/libs/icon/index.js
@@ -4,71 +4,78 @@
  * External dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
+import { addFilter } from '@wordpress/hooks';
 
 /**
- * External dependencies
+ * Internal dependencies
  */
-import { addFilter } from '@wordpress/hooks';
+import {
+	buildCustomIconDataUrl,
+	encodeCustomSvgIcon,
+	isStandaloneIconBlock,
+} from './custom-icon-utils';
 
 export const applyIconExtensionHook = () => {
 	addFilter(
 		'blockera.featureIcon.extension.uploadSVG.onChangeHandler',
 		'blockera-pro/icon/uploadSVG',
-		({
-			ref,
-			newValue,
-			encodeIcon,
-			effectiveItems,
-			handleOnChangeAttributes,
-			blockName,
-		}) => {
-			const { svgString, uploadSVG, ...rest } = newValue;
-			const encodedIconObj = encodeIcon(svgString, {
-				hasInlineStyle: true,
-				color: effectiveItems?.blockeraIconColor?.value,
-			});
-
-			if (blockName === 'blockera/icon') {
-				handleOnChangeAttributes(
-					'blockeraIcon',
-					{
-						...rest,
-						icon: '',
-						library: '',
-						renderedIcon: encodedIconObj.encodedIcon,
-					},
-					{
-						ref,
-						effectiveItems: {
-							...effectiveItems,
-							url:
-								'data:image/svg+xml;utf8,' +
-								encodedIconObj.icon,
-							alt: uploadSVG?.title
-								? sprintf(
-										// translators: %s is the icon name.
-										__('%s Icon', 'blockera'),
-										uploadSVG.title.replaceAll('-', ' ')
-								  )
-								: '',
-						},
-					}
-				);
-			} else {
-				handleOnChangeAttributes(
-					'blockeraIcon',
-					{
-						...rest,
-						icon: '',
-						library: '',
-						renderedIcon: encodedIconObj.encodedIcon,
-					},
-					{
-						ref,
-						effectiveItems,
-					}
-				);
+		(payload) => {
+			if (!payload || typeof payload !== 'object') {
+				return payload;
 			}
+
+			const {
+				ref,
+				newValue,
+				effectiveItems,
+				handleOnChangeAttributes,
+				blockName,
+				isIconBlock,
+			} = payload;
+
+			if (!newValue?.svgString || !handleOnChangeAttributes) {
+				return payload;
+			}
+
+			// Preserve custom SVG markup exactly; only encode for storage/transport.
+			const renderedIcon = encodeCustomSvgIcon(newValue.svgString);
+			const shouldSetIconBlockUrl =
+				isIconBlock ?? isStandaloneIconBlock(blockName);
+
+			handleOnChangeAttributes(
+				'blockeraIcon',
+				{
+					...newValue,
+					icon: '',
+					library: '',
+					renderedIcon: renderedIcon.encodedIcon,
+				},
+				{
+					ref,
+					effectiveItems: {
+						...effectiveItems,
+						...(shouldSetIconBlockUrl
+							? {
+									url: buildCustomIconDataUrl(
+										newValue.svgString
+									),
+									alt: newValue.uploadSVG?.title
+										? sprintf(
+												// translators: %s is the icon name.
+												__('%s Icon', 'blockera'),
+												newValue.uploadSVG.title.replaceAll(
+													'-',
+													' '
+												)
+										  )
+										: '',
+							  }
+							: {}),
+					},
+				}
+			);
+
+			return payload;
 		}
 	);
 };
