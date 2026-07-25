40ed5e218a0116998e0175a7d391f4ed7783261c
Ali Aghdam <betteraliaghdam@gmail.com>
2026-07-25 18:03:25 +0330

    refactor(controls-pro): remove custom SVG upload unlock filter
    - Drop customIcon.featureType hook now that free unlocks uploads
    - Keep getLibraryIcons.type unlock for Pro icon libraries

--- packages/controls-pro/js/icon-control/apply.js
+++ packages/controls-pro/js/icon-control/apply.js
@@ -4,14 +4,6 @@ import { addFilter } from '@wordpress/hooks';
 
 export const applyIconControlHooks = () => {
 	addFilter(
-		'blockera.controls.iconControl.customIcon.featureType',
-		'blockera-pro.controls.iconControl.customIcon.featureType',
-		() => {
-			return 'none';
-		}
-	);
-
-	addFilter(
 		'blockera.controls.iconControl.utils.getLibraryIcons.type',
 		'blockera-pro.controls.iconControl.utils.getLibraryIcons.type',
 		() => {
