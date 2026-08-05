bc27e56fa94144f6ad7838ef7cac4d54deeeb927
blockerabot <blockeraai+githubbot@gmail.com>
2026-08-05 09:10:03 +0000

    Sync shared packages from blockera

--- packages/dev-cypress/js/helpers.js
+++ packages/dev-cypress/js/helpers.js
@@ -16,4 +16,5 @@ export * from './helpers/missing-variable';
 export * from './helpers/storage';
 export * from './helpers/companion-plugin';
 export * from './helpers/site-editor-main-panel';
+export * from './helpers/blockera-ai';
 export * from './helpers/stub-companion-plugin-for-component';
