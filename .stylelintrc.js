3a6ed0def61cf4e51b59544838dbfd52a8d1de56
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-11 20:25:01 +0330

    refactor: consume shared lint/tooling configs from global-packages
    Thin-root shared configs with Pro overrides (text domains, freemius ignore,
    phpunit). Keep webpack guard rename. Bump global-packages to 74e27ea.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,36 +1,9 @@
-const postcssScss = require('postcss-scss');
-const wpConfig = require('@wordpress/stylelint-config');
+const base = require('./packages/global-packages/packages/dev-tools/js/stylelint/config');
 
 module.exports = {
-	...wpConfig,
-	plugins: [...(wpConfig.plugins || []), 'stylelint-scss'],
-	rules: {
-		...(wpConfig.rules || {}),
-		'function-url-quotes': 'always',
-		'no-descending-specificity': null,
-		'selector-class-pattern': null,
-		'color-hex-length': 'long',
-		'comment-empty-line-before': null,
-		'value-keyword-case': null,
-		'rule-empty-line-before': null,
-		'at-rule-no-unknown': null,
-		'scss/at-rule-no-unknown': true,
-		'at-rule-empty-line-before': [
-			'always',
-			{
-				except: ['blockless-after-blockless', 'first-nested'],
-				ignore: ['after-comment'],
-			},
-		],
-	},
+	...base,
 	ignoreFiles: [
-		...(wpConfig.ignoreFiles || []),
-		'coverage/**/*.css',
-		'packages/global-packages/packages/dev-cypress/**/*.css',
-		'packages/global-packages/packages/dev-cypress/**/*.scss',
-		'packages/global-packages/packages/dev-storybook/**/*.css',
-		'packages/global-packages/packages/dev-storybook/**/*.scss',
+		...(base.ignoreFiles || []),
 		'packages/blockera-pro-admin/js/style.scss',
 	],
-	customSyntax: postcssScss, // MUST be last to prevent override
 };
