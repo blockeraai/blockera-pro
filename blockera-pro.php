e631447584c5c382f7fee9bf53768ad85f8b66eb
Ali Aghdam <betteraliaghdam@gmail.com>
2026-08-20 04:32:58 +0330

    chore: update tested up to

--- blockera-pro.php
+++ blockera-pro.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://blockera.ai/products/site-builder/pricing/
  * Description: Unlock the full power of Blockera Site Builder with the PRO version.
  * Requires at least: 6.6
- * Tested up to: 6.8
+ * Tested up to: 7.1
  * Requires PHP: 7.4
  * Requires at least blockera: 1.12.2
  * Author: Blockera AI
