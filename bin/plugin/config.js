574ab33b980f5858990603805ed1b08e4a06d10e
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-29 15:58:24 +0330

    chore: drop other:changelog and versionMilestoneFormat
    The plugin CLI no longer registers a GitHub milestone changelog command.

--- bin/plugin/config.js
+++ bin/plugin/config.js
@@ -11,7 +11,6 @@ module.exports = createPluginCliConfig({
 	slug: 'blockera-pro',
 	name: 'Blockera PRO',
 	team: 'Blockeraai',
-	versionMilestoneFormat: '%(name)s %(major)s.%(minor)s',
 	githubRepositoryOwner: gitRepoOwner,
 	githubRepositoryName: 'blockera-pro',
 	pluginEntryPoint: 'blockera-pro.php',
