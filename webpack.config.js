b82f9bd3bcad1a056599428a119fbcd04d7a648e
Ali Aghdam <betteraliaghdam@gmail.com>
2026-09-15 02:05:42 +0330

    build: externalize @blockera/auth and @blockera/products

--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,10 @@ module.exports = createRootWebpackConfig({
 			'blockeraTelemetry_' + blockeraPackagesVersion.telemetry,
 		'@blockera/storage':
 			'blockeraStorage_' + blockeraPackagesVersion.storage,
+		'@blockera/auth':
+			'blockeraAuth_' + (blockeraPackagesVersion.auth || '1_0_0'),
+		'@blockera/products':
+			'blockeraProducts_' + blockeraPackagesVersion.products,
 		'@blockera/data': 'blockeraData_' + blockeraPackagesVersion.data,
 		'@blockera/utils': 'blockeraUtils_' + blockeraPackagesVersion.utils,
 		'@blockera/editor': 'blockeraEditor_' + blockeraPackagesVersion.editor,
